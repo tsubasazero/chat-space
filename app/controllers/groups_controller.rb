d2bb64ba4bfe572e4a8f9fd20bc7eae94d5cf70a
tsubasaito-zero <tsubasaito24@gmail.com>
2019-11-08 17:15:11 +0900

    マスター

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,10 @@
 class GroupsController < ApplicationController
     
-
     before_action :set_group, only: [:edit, :update]
 
     def index
     end
   
-
     def new
       @group = Group.new
       @group.users << current_user

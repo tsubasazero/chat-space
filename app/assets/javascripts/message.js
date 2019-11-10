28e5dc64f2de0747e3f53628d76c510d8ed9f5c6
tsubasaito-zero <tsubasaito24@gmail.com>
2019-11-10 19:00:24 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 function buildHTML(message){
-  if ( message.image ) {
-    img = message.image ? `<img src=${message.image}>` : "" ;
+  
+    var　img = message.image ? `<img src=${message.image}>` : "" ;
     var html =
      `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
@@ -18,9 +18,12 @@ function buildHTML(message){
         </div>
        ${img}
       </div>`
+     
     return html;
-  }
-}
+  };
+  
+  setInterval(reloadMessages, 5000);
+
 $('.js-form').on('submit', function(e){
 e.preventDefault();
 var formData = new FormData(this);
@@ -44,3 +47,34 @@ $.ajax({
   });
   return false;
 });
+
+if (window.location.href.match(/\/groups\/\d+\/messages/)){
+
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id =　$('.message:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    var insertHTML = '';
+    messages.forEach(function (message) {
+
+      //htmlを作り出して、それを変数に代入(作り出す処理は非同期の時に作った)
+              insertHTML = buildHTML(message); 
+      
+      //変数に代入されたhtmlをmessagesクラスにぶち込む
+              $('.messages').append(insertHTML);
+            })
+  })
+
+  .fail(function() {
+    console.log('error');
+  });
+};
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,9 @@
+json.(@message, :content, :image)
+json.created_at @message.created_at
+json.user_name @message.user.name
+#idもデータとして渡す
 json.id @message.id
 json.user_name @message.user.name
-json.date @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.content @message.content
 json.image @message.image_url

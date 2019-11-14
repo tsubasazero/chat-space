b49931bf1a5610326e521575b71cff8c7b36c9fc
tsubasaito-zero <tsubasaito24@gmail.com>
2019-11-14 21:19:21 +0900

    edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,9 +51,9 @@ $.ajax({
   
 });
 
-if (window.location.href.match(/\/groups\/\d+\/messages/)){
 
 var reloadMessages = function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
   last_message_id =　$('.message:last').data("message-id");
   $.ajax({
@@ -83,31 +83,3 @@ var reloadMessages = function() {
 };
 }
 })
-var reloadMessages = function() {
-  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-  last_message_id =　$('.message:last').data("message-id");
-  $.ajax({
-    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-    url: "api/messages",
-    //ルーティングで設定した通りhttpメソッドをgetに指定
-    type: 'get',
-    dataType: 'json',
-    //dataオプションでリクエストに値を含める
-    data: {id: last_message_id}
-  })
-  .done(function(messages) {
-    var insertHTML = '';
-    messages.forEach(function (message) {
-
-      //htmlを作り出して、それを変数に代入(作り出す処理は非同期の時に作った)
-              insertHTML = buildHTML(message); 
-      
-      //変数に代入されたhtmlをmessagesクラスにぶち込む
-              $('.messages').append(insertHTML);
-            })
-  })
-
-  .fail(function() {
-    console.log('error');
-  });
-};

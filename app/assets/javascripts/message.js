6963d8250454f99beba14b6a30643171f81a723e
tsubasaito-zero <tsubasaito24@gmail.com>
2019-11-14 19:53:30 +0900

    再提出

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-
+$(function() {
 function buildHTML(message){  
     var　img = message.image ? `<img src=${message.image}>` : "" ;
     var html =
@@ -24,7 +24,7 @@ function buildHTML(message){
   
   setInterval(reloadMessages, 5000);
 
-$('.js-form').on('submit', function(e){
+$('.new_message').on('submit', function(e){
 e.preventDefault();
 var formData = new FormData(this);
 var url = $(this).attr('action')
@@ -32,7 +32,7 @@ $.ajax({
   url: url,
   type: "POST",
   data: formData,
-  dataType: 'json',
+  dataType: "json",
   processData: false,
   contentType: false
 })
@@ -41,15 +41,48 @@ $.ajax({
    $('.messages').append(html);
    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
    $('form')[0].reset();
+   $('.form__submit').prop('disabled', false);
  })
   .fail(function(){
     alert('error');
+    // console.log('error');
+    return false;
   });
-  return false;
+  
 });
 
 if (window.location.href.match(/\/groups\/\d+\/messages/)){
 
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id =　$('.message:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    var insertHTML = '';
+    messages.forEach(function (message) {
+
+      //htmlを作り出して、それを変数に代入(作り出す処理は非同期の時に作った)
+              insertHTML = buildHTML(message); 
+      
+      //変数に代入されたhtmlをmessagesクラスにぶち込む
+              $('.messages').append(insertHTML);
+            })
+  })
+
+  .fail(function() {
+    console.log('error');
+  });
+};
+}
+})
 var reloadMessages = function() {
   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
   last_message_id =　$('.message:last').data("message-id");
